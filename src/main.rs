345d32b96ff36a2cb95b726db5ce1a9f490d2a4d
Pontus Laos <laos.pontus@gmail.com>
2021-05-22 08:47:43 +0200

    Nicer error handling

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,20 @@ use std::{
 use chrono::Utc;
 
 fn main() {
+    match run() {
+        Ok(path) => match std::fs::remove_file(path) {
+            Ok(_) => {}
+            Err(_) => exit(1),
+        },
+
+        Err(e) => {
+            eprintln!("{}", e.to_string());
+            exit(1);
+        }
+    }
+}
+
+fn run() -> Result<String, std::io::Error> {
     let args = std::env::args().skip(1).collect::<Vec<_>>();
     let datetime = Utc::now().naive_utc();
     let temp_file_path = format!("/tmp/bmv-{}", datetime.format("%Y%m%d%H%M%S"));
@@ -23,37 +37,18 @@ fn main() {
         Err(_) => "vim".into(),
     };
 
-    let mut temp_file = match OpenOptions::new()
+    let mut temp_file = OpenOptions::new()
         .write(true)
         .create(true)
-        .open(&temp_file_path)
-    {
-        Ok(file) => file,
-        Err(_) => {
-            eprintln!("Could not open file at path {}, exiting.", &temp_file_path);
-            exit(1);
-        }
-    };
+        .open(&temp_file_path)?;
 
     let args_str = args.join("\n");
 
-    if let Err(_) = temp_file.write_all(args_str.as_bytes()) {
-        eprintln!("Could not write bytes to file, exiting.");
-        exit(2);
-    }
+    temp_file.write_all(args_str.as_bytes())?;
 
-    if let Err(_) = Command::new(&editor).arg(&temp_file_path).status() {
-        eprintln!("Spawning {} failed, exiting.", &editor);
-        exit(3);
-    }
+    let _ = Command::new(&editor).arg(&temp_file_path).status()?;
 
-    let contents = match read_to_string(&temp_file_path) {
-        Ok(file_contents) => file_contents,
-        Err(_) => {
-            eprintln!("Could not read file {}, exiting.", &temp_file_path);
-            exit(4);
-        }
-    };
+    let contents = read_to_string(&temp_file_path)?;
     let contents = contents
         .trim_matches(|c| c == '\n')
         .split("\n")
@@ -62,13 +57,13 @@ fn main() {
     let old_len = args.len();
     let new_len = contents.len();
 
-    if old_len != new_len {
-        eprintln!("Wrong number of file names, exiting.");
-        exit(5);
+    if old_len == new_len {
+        for (old, new) in args.iter().zip(contents.iter()) {
+            println!("{} -> {}", old, new);
+            std::fs::rename(old, new)?;
+        }
     }
 
-    for (old, new) in args.iter().zip(contents.iter()) {
-        println!("{} -> {}", old, new);
-        std::fs::rename(old, new).unwrap();
-    }
+    // std::fs::remove_file(&temp_file_path)
+    Ok(temp_file_path)
 }
