421d1479e4f25fcb99262efa3a51d0733935e045
Pontus Laos <laos.pontus@gmail.com>
2021-05-29 22:19:30 +0200

    Remove the datetime part of the temp file.
    This was unnecessary, since the temp file always gets removed anyway.
    This also lead to the ability to remove chrono as a dependency. Also
    made the temp file path a constant, "TMP_FILE_PATH".

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::io::{self, Write};
 use std::{fs, process};
 
-use chrono::Utc;
 use clap::{crate_version, App, AppSettings, Arg};
 
 const NAME: &str = "bmv: Bulk Move";
 const ABOUT: &str = "This tool accepts any amount of file names, opens them in your $EDITOR and renames the ones you changed.";
 const DEFAULT_EDITOR: &str = "vim";
+const TMP_FILE_PATH: &str = "/tmp/bmvfile";
 
 fn main() {
     let options = App::new(NAME)
@@ -23,22 +23,20 @@ fn main() {
         .get_matches();
 
     let files = options.values_of("files").unwrap_or_default().collect();
-    let datetime = Utc::now().naive_utc();
-    let temp_file_path = format!("/tmp/bmv-{}", datetime.format("%Y%m%d%H%M%S"));
 
-    let result = run(&temp_file_path, files);
+    let result = run(files);
 
     if let Err(e) = &result {
         eprintln!("{}", e.to_string());
     }
 
-    match fs::remove_file(temp_file_path) {
+    match fs::remove_file(TMP_FILE_PATH) {
         Ok(_) if result.is_ok() => {}
         _ => process::exit(1),
     }
 }
 
-fn run(temp_file_path: &str, file_names: Vec<&str>) -> io::Result<()> {
+fn run(file_names: Vec<&str>) -> io::Result<()> {
     if file_names.len() == 0 {
         process::exit(0);
     }
@@ -53,17 +51,17 @@ fn run(temp_file_path: &str, file_names: Vec<&str>) -> io::Result<()> {
     let mut temp_file = fs::OpenOptions::new()
         .write(true)
         .create(true)
-        .open(&temp_file_path)?;
+        .open(&TMP_FILE_PATH)?;
 
     let args_str = file_names.join("\n");
 
     temp_file.write_all(args_str.as_bytes())?;
 
     let _ = process::Command::new(&editor)
-        .arg(&temp_file_path)
+        .arg(&TMP_FILE_PATH)
         .status()?;
 
-    let temp_file_contents = fs::read_to_string(&temp_file_path)?;
+    let temp_file_contents = fs::read_to_string(&TMP_FILE_PATH)?;
     let new_file_names = temp_file_contents
         .trim_matches(|c| c == '\n')
         .split("\n")
